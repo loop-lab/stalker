0054957faa612f32c0c8ce60925e9aafdead7b45
loop-lab <55971697+loop-lab@users.noreply.github.com>
2019-10-18 10:10:16 +0300

    Fixed page splitting

--- chapters.php
+++ chapters.php
@@ -1,7 +1,9 @@
 <?php
     ob_start();
     $url = 'http://'.$_SERVER['HTTP_HOST'];
-    $id_author = parse_url($url.$_SERVER['REQUEST_URI'], PHP_URL_QUERY);
+    $full_url = $url.$_SERVER['REQUEST_URI'];
+    parse_str(parse_url($full_url, PHP_URL_QUERY), $query);
+    $id_author = $query['id_author'];
     if ($id_author == '1') {
         $title = 'Байки Дыма';
     } elseif ($id_author == '2') {
@@ -36,10 +38,10 @@
                 <h1 class="title col-6"><a href="/chapters.html"><?=$title?></a></h1>
                 <ul class="nav col-6 justify-content-end align-items-center">
                     <li class="nav-item">
-                        <a class="nav-link active" href="/">Главная</a>
+                        <a class="nav-link active" href=<?=$url?>>Главная</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/chapters.html">Рассказы</a>
+                        <a class="nav-link" href=<?=$full_url?>>Рассказы</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Блог</a>
@@ -52,8 +54,8 @@
                         </svg>
                     </button>
                     <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-                        <a class="dropdown-item" href="/">Главная</a>
-                        <a class="dropdown-item" href="/chapters.html">Рассказы</a>
+                        <a class="dropdown-item" href=<?=$url?>>Главная</a>
+                        <a class="dropdown-item" href=<?=$full_url?>>Рассказы</a>
                         <a class="dropdown-item" href="#">Блог</a>
                     </div>
                 </div>
